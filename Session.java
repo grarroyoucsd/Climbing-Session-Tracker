d92feeba084eae1709556c1f7504c7cc0e470d86
Gareth Arroyo <grarroyo@ucsd.edu>
2026-02-06 23:08:09 -0800

    Refactor Session class and improve input handling

--- Session.java
+++ Session.java
@@ -16,7 +16,7 @@ public class Session
     {
         this.date = date;
         climbsList = new ArrayList<>();
-        grades = new int[15];
+        grades = new int[18];
     }
 
     public void finishSession() throws IOException
@@ -31,39 +31,79 @@ public class Session
 
     public void askForClimbs()
     {
-        Scanner input = new Scanner(System.in);
-
-        while (true)
+        try (Scanner input = new Scanner(System.in);)
         {
-            System.out.println("Enter name of climb or 'f' to finish: ");
-            String name = input.nextLine();
-            if (name.equals("f"))
+            while (true)
             {
-                break;
+                System.out.println("Enter name of climb or 'f' to finish: ");
+                String name = input.nextLine();
+                if (name.equals("f"))
+                {
+                    break;
+                }
+                
+                int grade;
+                while (true)
+                {
+                    System.out.println("Enter grade (0-17): ");
+                    if (input.hasNextInt())
+                    {
+                        grade = input.nextInt();
+                        input.nextLine();
+                        if (grade >= 0 && grade <= 17)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            System.out.println("Grade must be from 0-17");
+                        }
+                    }
+                    else
+                    {
+                        System.out.println("Enter a number from 0-17");
+                        input.nextLine();
+                    }
+                }
+
+                int attempts;
+                while (true)
+                {
+                    System.out.println("Enter number of attempts: ");
+                    if (input.hasNextInt())
+                    {
+                        attempts = input.nextInt();
+                        input.nextLine();
+                        if (attempts >= 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            System.out.println("Number of attempts must be positive");
+                        }
+                    }
+                    else
+                    {
+                        System.out.println("Enter a number");
+                        input.nextLine();
+                    }
+                }
+
+                // Work on!!!
+                System.out.println("Did you send the climb? (yes/no) ");
+                String sentYesNo = input.nextLine();
+                boolean sent = false;
+                if (sentYesNo.equals("yes"))
+                {
+                    sent = true;
+                    ++grades[grade];
+                }
+
+                Climb climb = new Climb(name, grade, attempts, sent);
+                climbsList.add(climb);
             }
-
-            System.out.println("Enter grade: ");
-            int grade = input.nextInt();
-            input.nextLine();
-
-            System.out.println("Enter # of attempts: ");
-            int attempts = input.nextInt();
-            input.nextLine();
-
-            System.out.println("Did you send the climb? (yes/no) ");
-            String sentYesNo = input.nextLine();
-            boolean sent = false;
-            if (sentYesNo.equals("yes"))
-            {
-                sent = true;
-                ++grades[grade];
-            }
-
-            Climb climb = new Climb(name, grade, attempts, sent);
-            climbsList.add(climb);
         }
-
-        input.close();
 
         try
         {
@@ -104,27 +144,51 @@ public class Session
         }
     }
 
+    public class protected Climb
+    {
+
+        private String name;
+        private int grade;
+        private int attempts;
+        private boolean sent;
+
+        public Climb(String name, int grade, int attempts, boolean sent)
+        {
+            this.name = name;
+            this.grade = grade;
+            this.attempts = attempts;
+            this.sent = sent;
+        }
+
+        @Override
+        public String toString()
+        {
+            String status = (sent) ? "Sent ✅" : "In Progress...";
+            return "Name: " + name + ", Grade: V" + grade +
+                "\nAttempts: " + attempts + " Status: " + status; 
+        }
+    }
+
     @Override
     public String toString()
     {
         String climbsListStr = "";
-        for (int i = 0; i < climbsList.length; ++i)
+        for (int i = 0; i < climbsList.size(); ++i)
         {
-            climbsListStr += "\n" + climbsList[i];
+            climbsListStr += "\n" + climbsList.get(i);
         }
         return date + climbsListStr;
     }
 
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
+        try(Scanner input = new Scanner(System.in);)
+        {
+            System.out.println("Enter date of session: ");
+            String date = input.nextLine();
+            Session sesh = new Session(date);
 
-        System.out.println("Enter date of session: ");
-        String date = input.nextLine();
-        Session sesh = new Session(date);
-
-        sesh.askForClimbs();
-
-        input.close();
+            sesh.askForClimbs();
+        }
     }
 }
